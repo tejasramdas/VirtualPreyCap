d8b3d48ce35dadf964fb5f95cd6a9164cf55f37f
larrylegend33 <andrewdbolton@fas.harvard.edu>
2021-10-15 00:05:25 -0400

    not the worst place in the world with the VR

--- vr.jl
+++ vr.jl
@@ -11,39 +11,39 @@ using LinearAlgebra
 #gain!(cam, 0)
 #acquisitionmode!(cam, "Continuous")
 
-gold = load(download("https://raw.githubusercontent.com/nidorx/matcaps/master/1024/E6BF3C_5A4719_977726_FCFC82.png"))
+#gold = load(download("https://raw.githubusercontent.com/nidorx/matcaps/master/1024/E6BF3C_5A4719_977726_FCFC82.png"))
 
 
-py"""
+# py"""
 
-import json
-import pandas as pd
-import sppl.compilers.spn_to_dict as spn_to_dict
-from sppl.transforms import Identity as I
-from collections import OrderedDict
+# import json
+# import pandas as pd
+# import sppl.compilers.spn_to_dict as spn_to_dict
+# from sppl.transforms import Identity as I
+# from collections import OrderedDict
 
-df = pd.read_csv('/Users/nightcrawler/inferenceql.auto-modeling/data/nullified.csv')
+# df = pd.read_csv('/Users/nightcrawler/inferenceql.auto-modeling/data/nullified.csv')
 
-def loader():
-    with open('/Users/nightcrawler/inferenceql.auto-modeling/data/sppl/merged.json') as f:
-        spn_dict = json.load(f)
-    model = spn_to_dict.spn_from_dict(spn_dict)
-    return model
+# def loader():
+#     with open('/Users/nightcrawler/inferenceql.auto-modeling/data/sppl/merged.json') as f:
+#         spn_dict = json.load(f)
+#     model = spn_to_dict.spn_from_dict(spn_dict)
+#     return model
 
-def generate(model, N):
-    samples = model.sample(N)
-    return pd.DataFrame(
-        [
-            {k.__str__():v
-             for k,v in sample.items()
-             if ('_cluster' not in k.__str__()) and k.__str__()!='child'
-            }
-        for sample in samples
-    ])[df.columns]
+# def generate(model, N):
+#     samples = model.sample(N)
+#     return pd.DataFrame(
+#         [
+#             {k.__str__():v
+#              for k,v in sample.items()
+#              if ('_cluster' not in k.__str__()) and k.__str__()!='child'
+#             }
+#         for sample in samples
+#     ])[df.columns]
 
-"""
-model = py"loader"()
-py"generate"(model, 1)
+# """
+# model = py"loader"()
+# py"generate"(model, 1)
 
 function generate_bout(mod)
     bout_sample = py"generate"(mod, 1)
@@ -78,7 +78,7 @@ fishimage = convert(Matrix{UInt8}, fishimage * 255)
 function draw_para_trajectory()
     xtraj = zeros(500)
     ytraj = -50:.2:50
-    ztraj = 20*ones(500)
+    ztraj = 30*ones(500)
     full_traj = zip(xtraj, ytraj, ztraj)
     return collect(full_traj)
 end
@@ -101,12 +101,16 @@ function make_ground_mesh(lim, radius)
     return uv_normal_mesh(rendered_mesh)
 end
 
+
+
+
 function make_VR_environment()
     black = RGBAf0(0, 0, 0, 0.0)
     row_res = 800
     col_res = 480
-    env_fig = Figure(resolution=(row_res, col_res))
+    env_fig = Figure(resolution=(row_res, col_res), outer_padding=-50)
     limval = 100
+    fish_origin = 100
     mesh_cover = load("stone.png")
     timenode = Node(1)
     para_trajectory = draw_para_trajectory()
@@ -116,18 +120,26 @@ function make_VR_environment()
     # perspectiveness variable is 0.0 for orthographic, 1.0 for perspective, .5 for intermediate
     # have to use SceneSpace for markerspace to get coords in dataunits.
     # note "msize" has been replaced by "markersize" 
-    env_axis = Axis3(env_fig[1,1], xtickcolor=black,
-                     viewmode=:fit, aspect=(1,1,1), perspectiveness=0.5, protrusions=0, limits=lim)
-    mesh!(env_axis, make_ground_mesh(limval, 10000), color=mesh_cover, shading=false)
-    scatter!(env_axis, lift(t -> coords(t), timenode), markersize=5, markerspace=SceneSpace)
+    # env_axis = Axis3(env_fig[1,1], xtickcolor=black,
+    #                  viewmode=:fit, aspect=(1,1,1), perspectiveness=0.5, protrusions=0, limits=lim)
+    env_axis = Axis3(env_fig[1,1], xtickcolor=black, 
+                     aspect=(1,1,1), perspectiveness=.5, limits=lim)
+
+
+
+    scatter!(env_axis, lift(t -> coords(t), timenode), markersize=3, markerspace=SceneSpace)
+  #  wireframe!(env_axis, make_ground_mesh(fish_origin/4, limval*2))
+    mesh!(env_axis, make_ground_mesh(0, 200), color=mesh_cover, shading=false)
     # set rotation_center as the eyeposition b/c otherwise it rotates around the origin of the grid (i.e. the lookat)
-    fish = cam3d!(env_axis.scene, eyeposition=Vec3f0(-limval, 0, 0), lookat=Vec3f0(0,0,0), fixed_axis=true, fov=100, rotation_center=:eyeposition)
-  #  hidedecorations!(env_axis)
-  #  hidespines!(env_axis)
+    fish = cam3d!(env_axis.scene, eyeposition=Vec3f0(-limval, 0, 0), lookat=Vec3f0(0,0,0), fixed_axis=true, fov=50, rotation_center=:eyeposition)
+    hidedecorations!(env_axis)
+    hidespines!(env_axis)
     center!(env_axis.scene)
     # this is the only way to center it and get it to the point you want!
     translate_cam!(env_axis.scene, fish, Vec3f0(0, 0, fish.eyeposition[][1] + limval))
-    translate_cam!(env_axis.scene, fish, Vec3f0(0, -fish.eyeposition[][3], 0))
+
+    # puts the fish up to 10 pix in vertical. 
+    translate_cam!(env_axis.scene, fish, Vec3f0(0, -fish.eyeposition[][3] + 10, 0))
     translate_cam!(env_axis.scene, fish, Vec3f0(fish.eyeposition[][2], 0, 0))
     display(env_fig)
     # for translate cam and rotate cam, the vectors are side to side, up and down, and into the screen. they aren't x, y, z, but are in the same units. angles are in rad.
@@ -153,16 +165,17 @@ function make_VR_environment()
         i += 1
         sleep(.01)
         dyaw = 0
-        if i % 50 == 0
-#            dyaw = rand(range(-.1, .1, step=.001))
- #           b_az = -.5
-  #          b_dist = 3
-            b_az, dyaw, b_dist = generate_bout(model)
-            uvec = [cos(b_az), sin(b_az)]
-            b_vec = b_dist * uvec
-            translate_cam!(env_axis.scene, fish, Vec3f0(b_vec[2], 0, -b_vec[1]))
-            rotate_cam!(env_axis.scene, fish, Vec3f0(0, -dyaw, 0))
-        end
+
+    # logic goes "tank is 2000 pixels wide. if 8 pix = 1mm, then the tank is 25 cm (250 mm). 
+        
+#         if i % 50 == 0
+#             b_az, dyaw, b_dist = generate_bout(model)
+# #            b_dist is in pixel space. keep there. 
+#             uvec = [cos(b_az), sin(b_az)]
+#             b_vec = b_dist * uvec
+#             translate_cam!(env_axis.scene, fish, Vec3f0(b_vec[2], 0, -b_vec[1]))
+#             rotate_cam!(env_axis.scene, fish, Vec3f0(0, -dyaw, 0))
+#         end
         push!(fish_position, fish.eyeposition[][1:2])
         push!(fish_rotation, dyaw)
         if i == 500
